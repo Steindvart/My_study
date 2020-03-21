62e6756a535c21265afcb75dbe5985087c3d71f1
Ivan Kalashnikov <steindvart.st@gmail.com>
2020-03-21 11:11:28 +0300

    Fixed addaptive layout in glossary and fig

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Kalashnikov Ivan'
 author = 'Kalashnikov Ivan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
